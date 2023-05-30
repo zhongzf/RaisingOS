40254a09ccd19a621645997b5e138d59b9f533d0
zhongzf <zhongzf@126.com>
2023-05-30 20:56:04 +0800

    feat: add VM

--- BOOT/Program.cs
+++ BOOT/Program.cs
@@ -1,22 +1,19 @@
 ﻿using System;
 using Uefi;
 
-namespace BOOT
+unsafe class Program
 {
-  unsafe class Program
+  static void Main() { }
+
+  [System.Runtime.RuntimeExport("EfiMain")]
+  static long EfiMain(IntPtr imageHandle, EFI_SYSTEM_TABLE* systemTable)
   {
-    static void Main() { }
-
-    [System.Runtime.RuntimeExport("EfiMain")]
-    static long EfiMain(IntPtr imageHandle, EFI_SYSTEM_TABLE* systemTable)
+    string hello = "Hello world!";
+    fixed (char* pHello = hello)
     {
-      string hello = "Hello world!";
-      fixed (char* pHello = hello)
-      {
-        systemTable->ConOut->OutputString(systemTable->ConOut, pHello);
-      }
-
-      while (true) ;
+      systemTable->ConOut->OutputString(systemTable->ConOut, pHello);
     }
+
+    while (true) ;
   }
 }
