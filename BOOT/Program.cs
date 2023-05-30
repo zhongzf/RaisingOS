c728339de077f0ab01611ebcd5556eb335e5ca26
zhongzf <zhongzf@126.com>
2023-05-30 21:17:17 +0800

    feat: firmware = "efi"

--- BOOT/Program.cs
+++ BOOT/Program.cs
@@ -8,7 +8,7 @@ unsafe class Program
   [System.Runtime.RuntimeExport("EfiMain")]
   static long EfiMain(IntPtr imageHandle, EFI_SYSTEM_TABLE* systemTable)
   {
-    string hello = "Hello world!";
+    string hello = "Hello World!";
     fixed (char* pHello = hello)
     {
       systemTable->ConOut->OutputString(systemTable->ConOut, pHello);
