7fdcc6bc95c991a5cb62b24b0a0aa7ef155be2a9
zhongzf <zhongzf@126.com>
2023-05-30 19:59:20 +0800

    feat: add BOOT project.

---
+++ BOOT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Uefi;
+
+namespace BOOT
+{
+  unsafe class Program
+  {
+    static void Main() { }
+
+    [System.Runtime.RuntimeExport("EfiMain")]
+    static long EfiMain(IntPtr imageHandle, EFI_SYSTEM_TABLE* systemTable)
+    {
+      string hello = "Hello world!";
+      fixed (char* pHello = hello)
+      {
+        systemTable->ConOut->OutputString(systemTable->ConOut, pHello);
+      }
+
+      while (true) ;
+    }
+  }
+}
