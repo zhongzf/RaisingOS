b27d74924e41947d8ae68353082a4968307d9abc
zhongzf <zhongzf@126.com>
2023-06-03 20:24:41 +0800

    feat: shell

--- BOOT/Program.cs
+++ BOOT/Program.cs
@@ -9,29 +9,102 @@ unsafe class Program
   [RuntimeExport("EfiMain")]
   static EFI_STATUS EfiMain(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE* systemTable)
   {
-    string hello = "Hello World with edk2-cs!";
-    fixed (char* pHello = hello)
-    {
-      systemTable->ConOut->OutputString(systemTable->ConOut, pHello);
-    }
+    systemTable->ConOut->ClearScreen(systemTable->ConOut);
+    EfiInit(systemTable);
 
+    Shell();
+    return (RETURN_STATUS)0;
+  }
+
+  public const int CommandMaxLength = 100;
+
+  protected static EFI_SYSTEM_TABLE* _systemTable;
+
+  private static void EfiInit(EFI_SYSTEM_TABLE* systemTable)
+  {
+    _systemTable = systemTable;
+    _systemTable->BootServices->SetWatchdogTimer(0, 0, 0, null);
+  }
+
+  public static void PutC(char c)
+  {
+    var s = stackalloc char[2];
+    s[0] = c;
+    _systemTable->ConOut->OutputString(_systemTable->ConOut, s);
+  }
+
+  public static void Puts(string s)
+  {
+    fixed (char* p = s)
+    {
+      _systemTable->ConOut->OutputString(_systemTable->ConOut, p);
+    }
+  }
+
+  public static char Getc()
+  {
     EFI_INPUT_KEY key;
-    var str = stackalloc char[3];
+    ulong waitidx;
+
+    _systemTable->BootServices->WaitForEvent(1, &(_systemTable->ConIn->WaitForKey), &waitidx);
+    while ((RETURN_STATUS)_systemTable->ConIn->ReadKeyStroke(_systemTable->ConIn, &key) > 0)
+      ;
+
+    return key.UnicodeChar;
+  }
+
+  public static int Gets(char* buf, int bufSize)
+  {
+    var i = 0;
+    for (; i < bufSize - 1;)
+    {
+      buf[i] = Getc();
+      PutC(buf[i]);
+      if (buf[i] == '\b')
+      {
+        i--;
+        continue;
+      }
+      else if (buf[i] == '\r')
+      {
+        PutC('\n');
+        break;
+      }
+      i++;
+    }
+    buf[i] = '\0';
+    return i;
+  }
+
+  public static bool StringCompare(string s1, char* s2)
+  {
+    fixed (char* p1 = s1)
+    {
+      for (int i = 0; i < s1.Length; i++)
+      {
+        if (p1[i] != s2[i])
+        {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  public static void Shell()
+  {
+    var command = stackalloc char[CommandMaxLength];
     while (true)
     {
-      systemTable->ConIn->ReadKeyStroke(systemTable->ConIn, &key);
-      if (key.UnicodeChar != '\r')
-      {
-        str[0] = key.UnicodeChar;
-        str[1] = '\0';
-      }
+      Puts("RaisingOS > ");
+
+      if (Gets(command, CommandMaxLength) <= 0)
+        continue;
+
+      if (StringCompare("hello", command))
+        Puts("Hello UEFI!\r\n");
       else
-      {
-        str[0] = '\r';
-        str[1] = '\n';
-        str[2] = '\0';
-      }
-      systemTable->ConOut->OutputString(systemTable->ConOut, str);
+        Puts("Command not found.\r\n");
     }
   }
 }
